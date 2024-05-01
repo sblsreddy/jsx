c2957d255297714fa6037e6e90510be16b03df8b
sblsreddy <r14141@gmail.com>
2024-05-01 16:00:09 -0500

    intial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
+// 1) Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
+// 2) Get a reference to the div with ID root
+const el = document.getElementById('root');
+
+// 3) Tell React to take control of that element
+const root = ReactDOM.createRoot(el);
+
+// 4) Create a component
+function App(){
+   const date = new Date();
+   const time = date.toLocaleDateString();
+   const name = 'Sridhar';
+
+    return <h1> Hi there {time} {name} <input type="number" min ={5}/><br/>
+    style={{ color :'Red'}}<br/></h1>;
+}
+
+// 5) Show the component on the screen
+root.render(<App/>);
+
+/* import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -10,8 +31,8 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+ */
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
